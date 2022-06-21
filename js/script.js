177031bc1ec6a95a334104d63b4423576b3bbbb3
Miracle K Emenike <52224580+miraclemenikelechi@users.noreply.github.com>
2022-06-21 13:23:58 +0100

    updates

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 const puzzle = document.querySelector(".puzzle"),
+    input = document.querySelectorAll(".box"),
     solve = document.querySelector(".solve"),
-    reset = document.querySelector(".clear");
+    reset = document.querySelector(".clear"),
+    theme = document.querySelector(".theme-container"),
+    openTheme = document.querySelector("#open-theme"),
+    closeTheme = document.querySelector("#close-theme"),
+    toggle = document.querySelector(".theme-toggle");
+
+toggle.onclick = () => {
+    toggle.classList.toggle("active");
+    if (toggle.classList.contains("active")) {
+
+    }
+};
+
+openTheme.onclick = () => {
+    theme.classList.add("active");
+    document.body.style.paddingRight("350px");
+};
+closeTheme.onclick = () => {
+    theme.classList.remove("active");
+    document.body.style.paddingRight("0px");
+};
+
+
+console.log(input);
